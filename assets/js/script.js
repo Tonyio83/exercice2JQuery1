5e659a75e9fe59c2dae07c96c045ece921035777
Tonyio83 <anthonyfrancelle@gmail.com>
2019-11-04 11:10:40 +0100

    exercice 2 terminé

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$('#Hide').click(function(){//déclare une variable pour le bouton Cacher et crée une fonction au click du bouton
+  $('#text').hide();//cache le texte
+})
+$('#Show').click(function(){//crée une variable du bouton Afficher et une fonction au clique du bouton
+  $('#text').show();//affiche le texte
+})
